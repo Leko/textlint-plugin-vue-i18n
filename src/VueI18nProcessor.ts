0ebffabb59568f3465e9d74d93ba9d9d86887c52
Leko <leko.noor@gmail.com>
2021-08-07 13:13:28 +0800

    feat: support resources option

--- src/VueI18nProcessor.ts
+++ src/VueI18nProcessor.ts
@@ -3,8 +3,10 @@ import type {
   TextlintPluginOptions,
 } from '@textlint/types'
 import { ASTNodeTypes, TxtNode } from '@textlint/ast-node-types'
-import { findI18nBlock, fromSFCBlock } from './vue-ast'
+import * as micromatch from 'micromatch'
+import { findI18nBlock, fromSFCBlock, rangeToLineLocation } from './vue-ast'
 import { getStringNodes, fromLiteralNode, pickLocales } from './json-ast'
+import { PropertyNode } from 'json-to-ast'
 
 function getEmptyDoc(): TxtNode {
   return {
@@ -22,28 +24,43 @@ function getEmptyDoc(): TxtNode {
 export class VueI18nProcessor implements TextlintPluginProcessor {
   config: TextlintPluginOptions
   locales: Set<string> | null
+  resources: string[]
 
   constructor(config = {}) {
     this.config = config
     this.locales = this.config['locales']
       ? new Set(this.config['locales'])
       : null
+    this.resources = this.config['resources'] ?? []
   }
 
   availableExtensions() {
-    return ['.vue']
+    return ['.vue', '.json']
   }
 
-  processor(_ext: string) {
+  processor() {
     const locales = this.locales
+    const resources = this.resources
     return {
-      preProcess(text: string, _filePath?: string): TxtNode {
-        const i18nBlock = findI18nBlock(text)
-        if (!i18nBlock) {
-          return getEmptyDoc()
+      preProcess(text: string, filePath?: string): TxtNode {
+        let rootNode: TxtNode, matches: PropertyNode[]
+        if (filePath && micromatch.isMatch(filePath, resources)) {
+          rootNode = {
+            type: ASTNodeTypes.Document,
+            raw: text,
+            range: [0, text.length],
+            loc: rangeToLineLocation(text, [0, text.length]),
+          }
+          matches = pickLocales(rootNode.raw, null)
+        } else {
+          const i18nBlock = findI18nBlock(text)
+          if (!i18nBlock) {
+            return getEmptyDoc()
+          }
+          rootNode = fromSFCBlock(text, i18nBlock)
+          matches = pickLocales(rootNode.raw, locales)
         }
-        const rootNode = fromSFCBlock(text, i18nBlock)
-        const matches = pickLocales(i18nBlock.content, locales)
+
         const children = matches
           .flatMap((m) => getStringNodes(m.value))
           .map((n) => fromLiteralNode(n, rootNode))
